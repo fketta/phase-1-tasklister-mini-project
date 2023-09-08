b4753000e566462e2469ea8955073daa7826bc9f
Fella Kettani <fellakettani@gmail.com>
2023-09-08 20:03:39 +0100

    partially finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+//const tasksArray = [];
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form")
+  form.addEventListener("submit", (e)=> {
+  e.preventDefault()
+  const A= e.target["new-task-description"].value
+  const B= e.target["priority"].value
+  // tasksArray.push({description: A, priority: B})
+  figuringThisOut(A,B)
+  form.reset()
+ })
 });
+
+// let asc = true
+// sortingButton.addEventListener('click', ()=> {
+//   asc=!asc
+// })
+
+function figuringThisOut (task, priority){
+  let p = document.createElement('p')
+  p.textContent = task + ' Priority: ' + priority
+  document.querySelector('#tasks').appendChild(p)
+
+  let btn = document.createElement('button')
+  btn.addEventListener('click', remover)
+  btn.textContent = "X"
+  btn.style.marginLeft = '5px'
+  p.appendChild(btn)
+
+  if (priority === 'low')
+  {p.style.color='green'}
+  if (priority === 'medium')
+  {p.style.color='yellow'}
+  if (priority === 'high')
+  {p.style.color='red'}
+}
+
+function remover(e){
+  e.target.parentNode.remove()
+}
